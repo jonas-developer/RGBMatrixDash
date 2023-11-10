9918c06f475bf100f3fe8cf64bc43cb66debcb48
Jonas <jonaskaz2009@outlook.de>
2023-11-10 22:02:07 +0100

    asddas

--- forpi/image.py
+++ forpi/image.py
@@ -6,38 +6,56 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 import requests
 import json 
-import urllib.request 
+import urllib.request
+import os
+import stat
 
-
-if len(sys.argv) < 2:
-    sys.exit("Require an image argument")
-else:
-    image_file = sys.argv[1]
-
-res = requests.get('http://192.168.178.21:9999/get-image')
-url = res.json()['image']
-print(url)
-urllib.request.urlretrieve(url, "image.png") 
-
-image = Image.open(image_file)
-
-# Configuration for the matrix
-options = RGBMatrixOptions()
-options.rows = 32
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
-
-matrix = RGBMatrix(options = options)
-
-# Make image fit our screen.
-image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-
-matrix.SetImage(image.convert('RGB'))
+image_file = ""
 
 try:
     print("Press CTRL-C to stop.")
     while True:
-        time.sleep(100)
+        time.sleep(5)
+        res = requests.get('http://192.168.178.21:9999/get-image')
+        url = res.json()['image']
+        print(url)
+        if os.path.isfile("./image.png") == True:
+            urllib.request.urlretrieve(url, "./image2.png") 
+            image_file = "./image2.png"
+            image = Image.open(image_file)
+
+            # Configuration for the matrix
+            options = RGBMatrixOptions()
+            options.rows = 32
+            options.cols = 64
+            options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+
+            matrix = RGBMatrix(options = options)
+
+            # Make image fit our screen.
+            image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+
+            matrix.SetImage(image.convert('RGB'))
+            os.chmod("./image.png",stat.S_IWOTH)
+            os.remove("./image.png")
+        else:
+            urllib.request.urlretrieve(url, "./image.png") 
+            image_file = "./image.png"
+            image = Image.open(image_file)
+
+            # Configuration for the matrix
+            options = RGBMatrixOptions()
+            options.rows = 32
+            options.cols = 64
+            options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+
+            matrix = RGBMatrix(options = options)
+
+            # Make image fit our screen.
+            image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+
+            matrix.SetImage(image.convert('RGB'))
+            os.chmod("./image2.png",stat.S_IWOTH)
+            os.remove("./image2.png")
 except KeyboardInterrupt:
     sys.exit(0)
--- forpi/test.py
+++ forpi/test.py
@@ -1,7 +1,54 @@
+#!/usr/bin/env python
+import time
+import sys
+
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image
 import requests
 import json 
-import urllib.request 
-res = requests.get('http://localhost:9999/get-image')
-url = res.json()['image']
-print(url)
-urllib.request.urlretrieve(url, "image.png") 
+import urllib.request
+import os
+import stat
+
+image_file1 = "/home/matrix/rpi-rgb-led-matrix/jonas/image.png"
+image_file2 = "/home/matrix/rpi-rgb-led-matrix/jonas/image2.png"
+try:
+    print("Press CTRL-C to stop.")
+    
+    # Configuration for the matrix
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.cols = 64
+    options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+    matrix = RGBMatrix(options=options)
+
+    while True:
+        time.sleep(5)
+        res = requests.get('http://192.168.178.21:9999/get-image')
+        url = res.json()['image']
+        print(url)
+
+        if os.path.isfile(image_file1):
+            with open(image_file2, 'wb') as f:
+                f.write(requests.get(url).content)
+            image = Image.open(image_file2)
+            with open(image_file1, 'wb') as f:
+                f.write(b"")  # Leeren Sie die Datei
+            os.chmod(image_file1, 0o777)
+            os.remove(image_file1)
+        else:
+            with open(image_file1, 'wb') as f:
+                f.write(requests.get(url).content)
+            image = Image.open(image_file1)
+            with open(image_file2, 'wb') as f:
+                f.write(b"")  # Leeren Sie die Datei
+            os.chmod(image_file2, 0o777)
+            os.remove(image_file2)
+
+        # Make image fit our screen.
+        image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+
+        matrix.SetImage(image.convert('RGB'))
+
+except KeyboardInterrupt:
+    sys.exit(0)
